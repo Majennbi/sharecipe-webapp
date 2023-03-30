3b6eea862f1cab7031c538dddc9bbcd7429bcfa6
Majennbi <85588366+Majennbi@users.noreply.github.com>
2023-03-30 16:06:19 +0200

    📋Creating database, 1st entity and fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Generator;
+use Faker\Factory;
+use App\Entity\Ingredient;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    /** 
+     * @var Generator 
+     * */
+
+    private Generator $faker;
+
+    public function __construct() 
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        //Ingredients
+        for ($i = 0; $i < 20; $i++) {
+            $ingredient = new Ingredient();
+            $ingredient->setName($this->faker->word());
+            $ingredient->setPrice(mt_rand(1, 100));
+            $manager->persist($ingredient);
+        }
+    
+        $manager->flush();
+    }
+}
